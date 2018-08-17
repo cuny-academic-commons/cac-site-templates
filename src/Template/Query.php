7b6730b72c8683d8e59fd740ced0244e4064b61c
Boone B Gorges <boonebgorges@gmail.com>
2018-08-17 11:37:02 -0500

    Fetch templates by menu_order by default.

--- src/Template/Query.php
+++ src/Template/Query.php
@@ -14,6 +14,7 @@ class Query {
 
 	public function get_results() {
 		$wp_query_args = [
+			'orderby'        => [ 'menu_order' => 'ASC' ],
 			'post_type'      => 'cac_site_template',
 			'posts_per_page' => -1,
 		];
